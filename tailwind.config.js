6f4c38a44405987ce53d79eeaabcc9b9025de1d1
NicholasVisentini <nicholas.santos@sptech.school>
2026-08-28 10:52:55 -0300

    FEAT: Tela de login e cadastro utilizando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        green950: '#0c1e1a',
+        green900: '#102420',
+        green700: '#1a3d36',
+        mist: '#f8fafc',
+        lineGray: '#e2e8f0',
+      }
+    },
+  },
+  plugins: [],
+}
