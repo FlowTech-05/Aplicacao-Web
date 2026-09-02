3d56a86c8339720ff607f7e70c01ae2aa99482fc
Matheus Vizzaccaro <matheus.goncalves@sptech.school>
2026-09-02 18:31:25 -0300

    feat: telas alterarem de acordo com a ação

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,9 @@
 let elementoPainel = document.getElementById("painel");
 
-function exibirPainel() {
+function exibirPainel(acaoTexto, cor) {
     elementoPainel.classList.remove("hidden");
+    document.getElementById('textoAcao').innerHTML = acaoTexto;
+    document.getElementById('botaoAcao').classList.add(`bg-${cor}-600/50`, `border-${cor}-600`)
 }
 
 function fecharPainel() {
