c9eea2a10922333f7eba2ead65ed55bfcd134ceb
Matheus Vizzaccaro <matheus.goncalves@sptech.school>
2026-09-02 19:16:36 -0300

    fix: botoes de interação com os cards de computador

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,13 @@
-let elementoPainel = document.getElementById("painel");
-
-function exibirPainel(acaoTexto, cor) {
-    elementoPainel.classList.remove("hidden");
+function exibirPainel(id, acaoTexto, cor) {
+    document.getElementById(id).classList.remove("hidden");
     document.getElementById('textoAcao').innerHTML = acaoTexto;
     document.getElementById('botaoAcao').classList.add(`bg-${cor}-600/50`, `border-${cor}-600`)
 }
 
-function fecharPainel() {
-    elementoPainel.classList.add("hidden");
+function exibirDelete(id) {
+    document.getElementById(id).classList.remove("hidden");
+}
+
+function fecharPainel(id) {
+    document.getElementById(id).classList.add("hidden");
 }
