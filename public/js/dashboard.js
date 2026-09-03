16b0a307cb86b6b8a4e9ac75ff94875674d936d3
Matheus Vizzaccaro <matheus.goncalves@sptech.school>
2026-09-02 21:36:43 -0300

    feat: responsividade da tela dash

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,7 @@
 function exibirPainel(id, acaoTexto, cor) {
     document.getElementById(id).classList.remove("hidden");
     document.getElementById('textoAcao').innerHTML = acaoTexto;
+    document.getElementById('botaoAcao').className = 'w-fit border-1 p-1 rounded-sm';
     document.getElementById('botaoAcao').classList.add(`bg-${cor}-600/50`, `border-${cor}-600`)
 }
 
