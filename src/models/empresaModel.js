1816ddef6ab0916a302156354c41102f49367809
Matheus Vizzaccaro <matheus.goncalves@sptech.school>
2026-09-03 23:53:03 -0300

    feat: adicionada a procedure na model

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,8 +1,7 @@
 var database = require("../database/config");
 
 function cadastrarEmpresa(cnpj, razao_social, nome_fantasia) {
-  var instrucaoSql = `INSERT INTO empresas(cnpj, razao_social, nome_fantasia) VALUES ('${cnpj}', '${razao_social}', '${nome_fantasia}')`;
-
+  var instrucaoSql = `CALL cadastrarEmpresa('${cnpj}', '${razao_social}', '${nome_fantasia}');`;
   return database.executar(instrucaoSql);
 }
 
